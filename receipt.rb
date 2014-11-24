543e20ee30bdfc6d02f3d3a93dde6270b1e072c1
kasei_san <kasei.san@gmail.com>
2014-11-24 13:28:08 +0900

    Add QR code

--- receipt.rb
+++ receipt.rb
@@ -10,10 +10,10 @@ class Recipt
     @serialport = SerialPort.new(path, 19200)
     @serialport.read_timeout = 1000
     @serialport.write "\e@" # ESC @ : Reset
-    control_paramer(10, 255, 255)
+    control_paramer
   end
 
-  def control_paramer(max_printing_dots, heating_time, heating_interval)
+  def control_paramer(max_printing_dots=10, heating_time=80, heating_interval=2)
     @serialport.write "\e7#{max_printing_dots.chr}#{heating_time.chr}#{heating_interval.chr}"
   end
 
@@ -24,7 +24,7 @@ class Recipt
   def image_write(path)
     bmp = Bmp.new(path)
 
-    buffer  = "              # {0x12.chr}v" # DC2 v : Print LSB Bitmap
+    buffer  = "#{0x12.chr}v" # DC2 v : Print LSB Bitmap
     buffer += bmp.height.chr # nL
     buffer += 0.chr          # nH
     buffer += bmp.image.map{|line| [line].pack("b*")}.join
@@ -37,9 +37,11 @@ class Recipt
 end
 
 r = Recipt.new('/dev/tty.usbserial-FTGD4019')
-
 r.text_write("--------------------------------\n")
 r.image_write("./name.bmp")
+r.text_write("--------------------------------\n")
+r.image_write("./qr.bmp")
+r.text_write("  How to make Cyber Sunglasses  \n")
 r.text_write("--------------------------------\n")
 r.text_write("\n")
 r.text_write("Web engineer(Ruby on Rails)\n")
@@ -49,7 +51,5 @@ r.text_write("\n")
 r.text_write("twitter  : @kasei_san\n")
 r.text_write("facebook : facebook.com/kaseisan\n")
 r.text_write("Qiita    : qiita.com/kasei-san\n")
-
 r.text_write("\n\n\n\n")
-
 r.close
