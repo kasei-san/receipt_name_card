9d7591627762b5d1e690f5b2fe2f0d2bcdeff675
kasei_san <kasei.san@gmail.com>
2014-11-24 13:49:51 +0900

    Separate Receipt class and main code

---
+++ main.rb
@@ -0,0 +1,19 @@
+require_relative 'receipt.rb'
+Recipt.new('/dev/tty.usbserial-FTGD4019') do
+  control_paramer(5, 160, 4)
+  text_write("--------------------------------\n")
+  image_write("./name.bmp")
+  text_write("--------------------------------\n")
+  image_write("./qr.bmp")
+  text_write("  How to make Cyber Sunglasses  \n")
+  text_write("--------------------------------\n")
+  text_write("\n")
+  text_write("Web engineer(Ruby on Rails)\n")
+  text_write("Maker\n")
+  text_write("Ninja Heads!\n")
+  text_write("\n")
+  text_write("twitter  : @kasei_san\n")
+  text_write("facebook : facebook.com/kaseisan\n")
+  text_write("Qiita    : qiita.com/kasei-san\n")
+  text_write("\n\n\n\n")
+end
--- receipt.rb
+++ receipt.rb
@@ -1,7 +1,7 @@
-require "rubygems"
+require 'rubygems'
 gem 'serialport','>=1.0.4'
-require "serialport"
-require "./bmp.rb"
+require 'serialport'
+require_relative 'bmp.rb'
 
 class Recipt
   attr_reader :serialport
@@ -36,23 +36,4 @@ class Recipt
   def close
     serialport.close
   end
-end
-
-Recipt.new('/dev/tty.usbserial-FTGD4019') do
-  control_paramer(5, 160, 4)
-  text_write("--------------------------------\n")
-  image_write("./name.bmp")
-  text_write("--------------------------------\n")
-  image_write("./qr.bmp")
-  text_write("  How to make Cyber Sunglasses  \n")
-  text_write("--------------------------------\n")
-  text_write("\n")
-  text_write("Web engineer(Ruby on Rails)\n")
-  text_write("Maker\n")
-  text_write("Ninja Heads!\n")
-  text_write("\n")
-  text_write("twitter  : @kasei_san\n")
-  text_write("facebook : facebook.com/kaseisan\n")
-  text_write("Qiita    : qiita.com/kasei-san\n")
-  text_write("\n\n\n\n")
 end
